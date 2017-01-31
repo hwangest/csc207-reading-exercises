a6e4efe80f3077a818a0b9a5ca68bb96c10cb029
Esther Y. Hwang <hwangest@cs.grinnell.edu>
2017-01-31 00:10:15 -0600

    Did Dogs exercise

---
+++ 01-30/src/Dogs.java
@@ -0,0 +1,24 @@
+/*
+ * @author hwangest
+ *
+ */
+
+public class Dog {
+    //three fields
+    public String name;
+    public String breed;
+    public int tailLength;
+
+    //constructor
+    public Dog (String name, String breed, int tailLength); {
+        this.name = name;
+        this.breed = breed;
+        this.tailLength = tailLength;
+        /* ... */
+    }
+    
+    //method 
+    public boolean pottytrained(String name) {
+        /* ... */
+    }
+        
