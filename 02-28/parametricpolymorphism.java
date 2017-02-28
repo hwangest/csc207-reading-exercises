0e542a2fafa40336ddd84356a32c9d5fa4c18274
Esther Y. Hwang <hwangest@cs.grinnell.edu>
2017-02-28 13:23:13 -0600

    reading exercise 02-28

---
+++ 02-28/parametricpolymorphism.java
@@ -0,0 +1,19 @@
+-/*
+-
+-Esther Hwang
+-02-28 Polymetric Polymorphism Reading Exercise
+-
+-*/
+-
+-public class Box<T> {
+-    private T value;
+-    public Box(T value) {
+-        this.value = value;
+-    }
+-    public T getValue() {
+-        return value;
+-    }
+-    public T setValue(T value) {
+-        this.value = value;
+-    }
+-}
