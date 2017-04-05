6537f902d5dac96a1af423042af0e8031ef83440
Esther Y. Hwang <hwangest@cs.grinnell.edu>
2017-04-04 20:38:02 -0500

    maybe just pushed reading exercise?

---
+++ 04-05/trees.java
@@ -0,0 +1,35 @@
+/*
+Esther Hwang
+Reading Exercise 4/5/2017
+
+(a)
+tree.add(5)
+
+tree.add(2)
+tree.add(8)
+
+tree.add(1)
+tree.add(4)
+tree.add(6)
+tree.add(9)
+
+Note that 2 and 8 could have been added in any order. Also, the last four values added to the tree, 1, 4, 6, and 9, could be called in any order.
+
+(b)
+tree.add(4)
+
+tree.add(2)
+tree.add(9)
+
+tree.add(15)
+tree.add(6)
+
+tree.add(20)
+tree.add(5)
+
+(c)
+tree.add(9)
+tree.add(6)
+tree.add(5)
+tree.add(2)
+tree.add(1)
